b07e21b8587e80b6c4217f1cfa70a8abfaf3a24d
soberlevi <7807081@qq.com>
2017-06-24 17:21:56 +0800

    add \s $ { } charset to regex

--- file-replace.js
+++ file-replace.js
@@ -5,7 +5,6 @@ let fs = require('fs');
 
 function fileReplace(file, patterns, callback) {
     let data = fs.readFileSync(file).toString().split('\n');
-    console.log(patterns);
     patterns = JSON.parse(patterns);
 
     data.forEach((line, index) => {
@@ -13,7 +12,7 @@ function fileReplace(file, patterns, callback) {
             return line.search(pattern.k + ":") != -1;
         });
         if (pattern) {
-            let regexp = new RegExp('(' + pattern.k + ':[\\s])([\\w\"\':\\/\\/\.\-]+)(,{0,1})');
+            let regexp = new RegExp('(' + pattern.k + ':[\\s])([\\w\\s${}\"\':\\/\\/\.\-]+)(,{0,1})');
             line = line.replace(regexp, (m, p1, p2, p3) => {
                 let result = p1;
                 if (typeof pattern.v == 'string') {
